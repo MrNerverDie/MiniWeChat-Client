52cd8e13083d730c026eeadff3fbdf67146cd335
王选易 <wangxuanyihaha@gmail.com>
2015-03-21 10:53:10 +0800

    【新功能】添加新Panel

---
+++ Assets/Script/Common/WelcomePanel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MiniWeChat
+{
+    public class WelcomePanel : BaseState {
+
+        public override void OnEnter(object param = null)
+        {
+
+        }
+
+        public override void OnExit()
+        {
+
+        }
+    }
+}
+
