8cd2360b8fedb08486cead9c48fc432d700a28d3
王选易 <wangxuanyihaha@gmail.com>
2015-03-26 12:48:15 +0800

    【功能优化】变更目录结构

---
+++ Assets/Script/Model/GlobalContacts.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using protocol;
+
+namespace MiniWeChat
+{
+    public class GlobalContacts : Singleton<GlobalContacts>
+    {
+        private List<UserItem> _friendList;
+
+        public override void Init()
+        {
+            base.Init();
+        }
+
+        public override void Release()
+        {
+            base.Release();
+        }
+
+        
+    }
+}
+
