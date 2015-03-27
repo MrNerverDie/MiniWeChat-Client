e6255ce4cecc28d578fcdd0e84e900de44854603
王选易 <wangxuanyihaha@gmail.com>
2015-03-27 12:12:13 +0800

    【功能优化】Dotween升级

--- Assets/Script/Model/Base/BaseState.cs
+++ Assets/Script/Model/Base/BaseState.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using DG.Tweening;
 
 namespace MiniWeChat
 {
+    [RequireComponent(typeof(CanvasGroup))]
     public class BaseState : MonoBehaviour
     {
+
+        private CanvasGroup _canvasGroup;
+
         /// <summary>
         /// 界面栈初始化的时候被调用的方法
         /// </summary>
         /// <param name="param">需要传进来的参数</param>
         public virtual void OnEnter(object param = null)
         {
+            _canvasGroup = GetComponent<CanvasGroup>();
             OnShow();
+            transform.localScale = Vector3.one * 0.8f;
+            transform.DOScale(Vector3.one, 0.3f);
+            _canvasGroup.alpha = 0f;
+            DOTween.To(() => _canvasGroup.alpha, x => _canvasGroup.alpha = x, 1.0f, 0.3f);
         }
 
         /// <summary>
@@ -21,6 +31,8 @@ namespace MiniWeChat
         public virtual void OnExit()
         {
             OnHide();
+            transform.localScale = Vector3.one;
+            transform.DOScale(Vector3.zero, 0.3f);
         }
 
         /// <summary>
