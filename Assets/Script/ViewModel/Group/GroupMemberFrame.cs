4998e4f4fd088a83d7f44774a22c6a90021131ee
王选易 <wangxuanyihaha@gmail.com>
2015-04-17 13:15:07 +0800

    【新功能】初步完成了群组所需要的新界面

---
+++ Assets/Script/ViewModel/Group/GroupMemberFrame.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using protocol;
+
+namespace MiniWeChat
+{
+    public class GroupMemberFrame : MonoBehaviour
+    {
+        public Image _imageHead;
+        public Text _labelUserName;
+
+        private UserItem _userItem;
+
+        public void Show(UserItem userItem)
+        {
+            _userItem = userItem;
+
+            if (_userItem != null)
+            {
+                UIManager.GetInstance().SetImage(_imageHead, EAtlasName.Head, "00" + _userItem.headIndex);
+                _labelUserName.text = _userItem.userName;
+            }
+        }
+
+        
+    }
+}
+
